ea55c7946f7b81e09e44a705faee50d6f12e0939
TOM <351245952>
2021-06-10 23:20:14 +0800

    增加项目2

---
+++ javaProject02/src/main/java/com/spring/com/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.spring.com;
+
+/**
+ * @author kylinWang
+ * @data 2021/1/8 22:49
+ */
+public class HelloWorld {
+    public void sayHello(){
+        System.out.println("hello spring");
+    }
+}
---
+++ javaProject02/src/main/java/com/spring/com/TestHelloWorld.java
@@ -0,0 +1,36 @@
+package com.spring.com;
+
+import org.junit.Test;
+
+/**
+ * @author kylinWang
+ * @data 2021/1/8 22:54
+ */
+public class TestHelloWorld {
+    @Test  //需要一些比较基础的东西
+    public  void test(){
+        //面向切面编程
+
+        //集合 Array , list , set , map
+
+        //编码与反编码
+
+        //注解，请求 sprinng + 微服务 + redis 等才是一个完整的项目
+
+        //2. mybatis 是持久层框架， 支持 jdbc 的完美融合
+
+        //Xml 文件 ， 半自动的 mapping 框架
+
+        //3. springBoot 项目：快速启动Web 项目， 容器 Servlet
+
+
+        //动态页面， 动态分区， 等
+
+        //多思考
+
+        //将java的spring 好好理解一些
+
+
+
+    }
+}
